cb9d374242f8173f523b8203e24bb754f5827732
Rafael Diaz <Rafael Diaz@Rafa-PC>
2023-11-21 03:06:03 -0300

    tallerBiciciletas v1.6 Agrege informacion a "estado de bicicletas" faltan: boton egresar cliente Balances Manuales y ayuda Estetica

--- src/controlador/RegistrarCliente.java
+++ src/controlador/RegistrarCliente.java
@@ -54,26 +54,36 @@ public class RegistrarCliente {
         }
     }
 
-    public boolean egresarCliente(int id) {
+    public boolean actualizarCliente(Cliente cli) {
         Connection cnx = null;
         try {
 
             bd cbd = new bd();
             cnx = cbd.obtenerConeccion();
 
-            String query = "DELETE FROM cliente WHERE id=?";
-            PreparedStatement stmt = cnx.prepareStatement(query);
-            stmt.setInt(1, id);
+                       String query = "UPDATE INTO cliente(idCliente,nombre,apellido,celular,fecha,modalidad,modelo,tipoServicio,valor,estado ) VALUES (?,?,?,?,?,?,?,?,?,?)";
+            try (PreparedStatement stmt = cnx.prepareStatement(query)) {
 
-            stmt.executeUpdate(); //elimina el cliente
-            stmt.close();
+                stmt.setInt(1, cli.getIdCliente());
+                stmt.setString(2, cli.getNombre());
+                stmt.setString(3, cli.getApellido());
+                stmt.setInt(4, cli.getCelular());
+                stmt.setDate(5, (java.sql.Date) cli.getFecha());
+                stmt.setString(6, cli.getModalidad());
+                stmt.setString(7, cli.getModelo());
+                stmt.setString(8, cli.getTipoServicio());
+                stmt.setInt(9, cli.getValor());
+                stmt.setString(10, cli.getEstado());
+
+                stmt.executeUpdate(); //actualiza cliente
+            }
             cnx.close();
             return true;
         } catch (SQLException e) {
-            System.out.println("Error SQL al eliminar cliente" + e.getMessage());
+            System.out.println("Error SQL al egresar Cliente" + e.getMessage());
             return false;
         } catch (Exception e) {
-            System.out.println("Error al eliminar cliente" + e.getMessage());
+            System.out.println("Error al egresar Cliente" + e.getMessage());
             return false;
         }
     }
@@ -123,7 +133,7 @@ public class RegistrarCliente {
         return lista;
     }
 
-    //buscar por fecha
+    //buscar por modalidad
     public List<Cliente> buscarPorModalidad(String modalidad) {
         List<Cliente> lista = new ArrayList<>();
 
@@ -167,4 +177,50 @@ public class RegistrarCliente {
         }
         return lista;
     }
+    
+    //egresar cliente
+       public boolean egresarCliente(Cliente cli) {
+        List<Cliente> lista = new ArrayList<>();
+
+        Connection cnx = null;
+        try {
+
+            bd cbd = new bd();
+            cnx = cbd.obtenerConeccion();
+
+            String query = "SELECT idCliente,nombre,apellido,celular,fecha,modalidad,modelo,tipoServicio,valor,estado  FROM cliente WHERE modalidad = ? order by idCliente ";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+    
+
+       stmt.setString(1, modalidad);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Cliente cli = new Cliente();
+
+           cli.setIdCliente(rs.getInt("idCliente"));
+             cli.setNombre(rs.getString("nombre"));
+             cli.setApellido(rs.getString("apellido"));
+             cli.setCelular(rs.getInt("celular"));
+             cli.setFecha(rs.getDate("fecha"));
+             cli.setModalidad(rs.getString("modalidad"));
+             cli.setModelo(rs.getString("modelo"));
+             cli.setTipoServicio(rs.getString("tipoServicio"));
+             cli.setValor(rs.getInt("valor"));
+             cli.setEstado(rs.getString("estado"));
+
+                lista.add(cli);
+            }
+            rs.close();
+            stmt.close();
+            cnx.close();
+        } catch (SQLException e) {
+            System.out.println("Error SQL al listar Cliente por fecha" + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error al mostrar Cliente por fecha" + e.getMessage());
+        }
+        return lista;
+    }
+    
 }
